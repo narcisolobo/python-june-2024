94265088a27c8cc6b621d5e88465b871ccb84088
narcisolobo <narcisolobo@gmail.com>
2024-06-26 23:11:48 -0700

    not sure

--- w1.02-fundamentals/11-nested-dicts-and-lists/script.py
+++ w1.02-fundamentals/11-nested-dicts-and-lists/script.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+
 """
 === === NESTED DICTIONARIES & LISTS === ===
 Nesting is also allowed in dictionaries. In other words,
@@ -37,7 +39,10 @@ instrument_brands = {
 
 # Getting and Setting Values in Nested Lists
 # Print "Deering".
+print(instrument_brands["banjos"][0])
 # Change "PRS" to "Paul Reed Smith".
+instrument_brands["electric_guitars"][2] = "Paul Reed Smith"
+pprint(instrument_brands)
 
 """
 === === LET'S LOOP! === ===
@@ -45,21 +50,42 @@ instrument_brands = {
 
 # Loop through the Monty Python list, printing each key of
 # each nested dictionary
+for each_actor in monty_python:
+    for key in each_actor:
+        print(key)
 
 # Loop through the Monty Python list, printing each value of
 # each nested dictionary
+for each_actor in monty_python:
+    for value in each_actor.values():
+        print(value)
 
 # Loop through the Monty Python list, printing each key and
 # value of each nested dictionary
+for each_actor in monty_python:
+    output = ""
+    for key, value in each_actor.items():
+        output += f"{key}: {value}, "
+    print(output[0:-2])
 
 # Loop through the instrument_brands dictionary, printing
 # the length of each nested list
+for item in instrument_brands:
+    print(len(instrument_brands[item]))
 
 # Loop through the instrument_brands dictionary, printing
 # each value of each nested list
+for type in instrument_brands:
+    for element in instrument_brands[type]:
+        print(element)
 
 # Loop through the instrument_brands dictionary, printing
 # the length of each nested list, each key, and each value
+for type in instrument_brands:
+    print(len(instrument_brands[type]), type)
+    for element in instrument_brands[type]:
+        print(element)
+
 
 # Expected output:
 """
